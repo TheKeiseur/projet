89f5b87dd21cee75e5aca1ffe9e62d78bc7039f5
TheKeiseur <arthur.sicard@gmail.com>
2021-05-17 15:11:50 +0000

    inscription fonctionnelle

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,9 +13,9 @@
     @foreach($posts as $post)
         <article>
             <header>
-                <h3>{{ $post->title }}</h3>
+                <h3><a href="#">{{ $post->title }}</a></h3>
+                <small>Question posée par {{ $post->user->name }} le {{ $post->created_at->format('d/m/Y H:i') }}</small>
             </header>
+            {!! nl2br(e($post->content)) !!}
         </article>
-
-
 @endsection
