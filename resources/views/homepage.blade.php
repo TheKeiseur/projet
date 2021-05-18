579deab3d3f1398a2254b01cdba9d966c8d9f23a
TheKeiseur <arthur.sicard@gmail.com>
2021-05-18 09:49:19 +0000

    ajout liens articles sur homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -11,7 +11,7 @@
         @foreach($posts as $post)
             <article>
                 <header>
-                    <h3><a href="#">{{ $post->title }}</a></h3>
+                    <h3><a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }}</a></h3>
                     <small>Question posée par {{ $post->user->name }} le {{ $post->created_at->format('d/m/Y H:i') }}</small>
                 </header>
                 <p>{!! Str::limit(nl2br(e($post->content)), 150, '(...)') !!}</p>
