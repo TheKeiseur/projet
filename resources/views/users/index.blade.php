9ef826efe1d1320ad89a82bddd0480d4fd435969
Hugo <hugocollebrusco@hotmail.fr>
2021-05-19 08:48:06 +0000

    filtre utilisateurs ajax correct

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,31 +1,33 @@
 @extends('layout')
 
-@section('title', 'Liste des utilisateurs')
+@section('title', 'Utilisateurs')
 
 @section('content')
 
-<h1>Liste des utilisateurs</h1>
-
-<form id="search-user" class="form-inline mb-3">
+    <h1>Liste des utilisateurs</h1>
+    
+    <form id="search-user" class="form-inline mb-3">
     <label for="search" class="sr-only">Filtrer</label>
     <input type="search" id="search" name="search" class="form-control" placeholder="Nom utilisateur">
-</form>
+    </form>
 
-<table id="user-list" class="table table-striped">
-    <thead>
-        <tr>
-            <th>Pseudo</th>
-            <th>Prénom</th>
-            <th>Nom</th>
-            <th>Email</th>
-            <th>Date de création</th>
-        </tr>
-    </thead>
-    <tbody>
-        @include('partials.users.index', ['users' => $users])
-    </tbody>
-</table>
-
-
-
-@endSection
+    <table id="user-list" class="table table-striped">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Email</th>
+                <th>Date de création</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->created_at }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    
+@endsection
